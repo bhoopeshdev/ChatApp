6591024be6656a20808ce6efd0bf5cf8b52cb4e4
Bhoopesh Dewangan <dew@synopsys.com>
2020-06-14 12:13:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+class App extends Component {
+    render() {
+        return(
+            <Text>Hi World</Text>
+        );
+    }
+}
+
+module.exports = App;
